a9c8456f17f6e4988902d36376a5b7fcad79f682
Soleil <soleil@macbookpro.lan>
2025-04-03 22:16:16 -0700

    updating eslint

--- registration-app/eslint.config.js
+++ registration-app/eslint.config.js
@@ -3,10 +3,29 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], 
+    languageOptions: { 
+      globals: globals.browser 
+    } 
+  },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/triple-slash-reference": "warn"
+    }
+  },
+  {
+    ignores: [".sst/**/*"]
+  }
 ]);
